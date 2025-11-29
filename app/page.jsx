61fd2ee0fcc781cdbd85ad7a18fa60a686da2506
clinicglobal <clinicglobalbd@gmail.com>
2025-11-29 18:36:57 -0300

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding: "40px", fontSize: "26px", textAlign: "center"}}>
-      <h1>ClinicGlobal</h1>
-      <p>Sistema instalado com sucesso!</p>
-    </div>
-  );
-}
