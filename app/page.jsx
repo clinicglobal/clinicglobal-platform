f06dbe41615cc7d458e969046f40017371ab7eeb
clinicglobal <clinicglobalbd@gmail.com>
2025-11-30 14:47:09 -0300

    Delete app/page.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ padding: '40px', fontFamily: 'Arial' }}>
-      <h1>Clinic Global Platform</h1>
-      <p>Seu sistema está funcionando corretamente!</p>
-    </div>
-  );
-}
