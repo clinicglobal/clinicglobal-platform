e5c1c3f3aad615ae55c693d8550cb8ede2cc06a3
clinicglobal <clinicglobalbd@gmail.com>
2025-12-01 03:18:53 -0300

    Create page.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

---
+++ page.jsx
@@ -0,0 +1,15 @@
+export default function ClinicPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "32px", color: "#4c1d95", fontWeight: "bold" }}>
+        Área da Clínica
+      </h1>
+
+      <p style={{ marginTop: "20px", fontSize: "18px", color: "#444" }}>
+        Bem-vinda à área interna da sua Clínica.
+        <br />
+        Aqui futuramente terá login, painel e funções exclusivas.
+      </p>
+    </div>
+  );
+}
