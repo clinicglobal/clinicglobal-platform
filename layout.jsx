25dcb618f6f70e1231554dd7eb5e6787f4374e9a
clinicglobal <clinicglobalbd@gmail.com>
2025-12-01 05:04:40 -0300

    Delete layout.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

--- layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoginLayout({ children }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-purple-50">
-      {children}
-    </div>
-  );
-}
