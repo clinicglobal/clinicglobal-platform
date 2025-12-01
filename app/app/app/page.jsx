c9686b0efc0b5a9c8dfcf0c1b81f2b43e6e3a0ab
clinicglobal <clinicglobalbd@gmail.com>
2025-12-01 02:51:18 -0300

    Create page.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

---
+++ app/app/app/page.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "36px", color: "#6a00ff" }}>
+        ClinicGlobal Platform
+      </h1>
+
+      <p style={{ marginTop: "20px", fontSize: "20px" }}>
+        Página inicial funcionando corretamente!
+      </p>
+    </div>
+  );
+}
