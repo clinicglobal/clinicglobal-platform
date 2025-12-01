4174b07492ba182c2587715192226f37ba09f36a
clinicglobal <clinicglobalbd@gmail.com>
2025-12-01 03:11:19 -0300

    Create page.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

---
+++ app/avaliacao/page.jsx
@@ -0,0 +1,13 @@
+export default function AvaliacaoPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "32px", color: "#4c1d95", fontWeight: "bold" }}>
+        Avaliação Facial Online
+      </h1>
+
+      <p style={{ marginTop: "20px", fontSize: "18px", color: "#444" }}>
+        Aqui você vai colocar seu formulário inteligente de avaliação facial Bonita Derme.
+      </p>
+    </div>
+  );
+}
