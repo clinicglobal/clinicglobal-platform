4fe176a88e82568ebba56dd87828f1c79ded872b
clinicglobal <clinicglobalbd@gmail.com>
2025-12-01 21:06:11 -0300

    Delete page.jsx
    Signed-off-by: clinicglobal <clinicglobalbd@gmail.com>

--- page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>BonitaDerme Clinic Global</h1>
-      <p>Plataforma carregada com sucesso.</p>
-    </div>
-  );
-}
